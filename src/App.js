ffefc0facc1a9fae61e682e0343a7cad1c616ce2
Alneed Roy <alneedrs2@gmail.com>
2022-03-26 04:39:53 +0600

    two question answer added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Header/Header";
 import AllBooks from "./AllBooks/AllBooks";
+import QA from "./Q&A/Q&A";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <AllBooks></AllBooks>
+      <QA></QA>
     </div>
   );
 }
