7b18efc0d04044f1271f81227849636481c5d6cf
Alneed Roy <alneedrs2@gmail.com>
2022-03-26 13:32:15 +0600

    no waning found

--- src/Q&A/Q&A.js
+++ src/Q&A/Q&A.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { ListGroup } from "react-bootstrap";
 
 const QA = () => {
   return (
@@ -8,26 +7,19 @@ const QA = () => {
         <h1 className="ms-2">
           What is the difference between Props and State?
         </h1>
-        <p className="mt-3">
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              ১. Props হলো Read Only অর্থাৎ অপরিবর্তনশীল। অন্যদিকে State এর
-              পরিবর্তন হয় ashynchronus ভাবে।{" "}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              ২. State মিউটেবল হলেও Props মিউটেবল নয়।
-            </ListGroup.Item>
-            <ListGroup.Item>
-              ৩. Props এক component থেকে অন্য component এ বিভিন্ন শর্ত সাপেক্ষে
-              ডাটা pass করে। অন্যদিকে State যেই component এ রাখা হয় শুধু তার
-              তথ্য দিতে পারে।
-            </ListGroup.Item>
-            <ListGroup.Item>
-              ৪. Props কে তার parents component কে pass করা যায়। অপরদিকে, State
-              শুধু তার নিজস্ব component কে ধারণ করে, অর্থাৎ এটি আর কোনো
-              component এ যায়না।
-            </ListGroup.Item>
-          </ListGroup>
+        <p className="mt-3 ms-2">
+          ১. Props হলো Read Only অর্থাৎ অপরিবর্তনশীল। অন্যদিকে State এর পরিবর্তন
+          হয় ashynchronus ভাবে। <br />
+          ২. State মিউটেবল হলেও Props মিউটেবল নয়।
+          <br />
+          ৩. Props এক component থেকে অন্য component এ বিভিন্ন শর্ত সাপেক্ষে ডাটা
+          pass করে। অন্যদিকে State যেই component এ রাখা হয় শুধু তার তথ্য দিতে
+          পারে।
+          <br />
+          ৪. Props কে তার parents component কে pass করা যায়। অপরদিকে, State শুধু
+          তার নিজস্ব component কে ধারণ করে, অর্থাৎ এটি আর কোনো component এ
+          যায়না।
+          <br />
         </p>
       </div>
       <div className="mt-5">
@@ -39,10 +31,10 @@ const QA = () => {
           রিঅ্যাক্ট এটাকে এমনভাবে তৈরী করে যে এই ডমের সাথে সহজেই জাভাস্ক্রিপ্ট
           ইউজ করে যেকোনো ইলিমেন্ট মডিফাই করা যায় কম সময়ের মধ্যে। এখন আগেরটার
           সাথে ভার্চুয়াল ডমের তুলনা করে রিঅ্যাক্ট দেখে আসলে নতুন কোন ইলিমেন্ট
-          পরিবর্তন করা হয়েছে। সে অনুসারে রিঅ্যাক্ট পুরো ডমটাকে রি-রেন্ডার না করে শুধুমাত্র যেই জিনিসটা চেঞ্জ হয়েছে সেটাকেই চেঞ্জ করে।
-          এতে সহজেই দ্রুত কাজ হয়ে যায় আর অতিরিক্ত, অপ্রয়োজনীয় কোনো কাজও করতে হয়
-          না ব্রাউজারের। আর এভাবেই, এই টেকনিক ইউজ করেই রিঅ্যাক্ট এতো দ্রুত 
-          রেন্ডার করে কাজ করে।
+          পরিবর্তন করা হয়েছে। সে অনুসারে রিঅ্যাক্ট পুরো ডমটাকে রি-রেন্ডার না করে
+          শুধুমাত্র যেই জিনিসটা চেঞ্জ হয়েছে সেটাকেই চেঞ্জ করে। এতে সহজেই দ্রুত
+          কাজ হয়ে যায় আর অতিরিক্ত, অপ্রয়োজনীয় কোনো কাজও করতে হয় না ব্রাউজারের।
+          আর এভাবেই, এই টেকনিক ইউজ করেই রিঅ্যাক্ট এতো দ্রুত রেন্ডার করে কাজ করে।
         </p>
       </div>
     </div>
